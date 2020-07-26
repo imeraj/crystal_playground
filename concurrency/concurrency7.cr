ec58a7ccb778e8dc37196396694b5e756ce036ea
Meraj <meraj.enigma@gmail.com>
2020-07-26 01:55:21 -0400

    - mutex lock/unlock example added

---
+++ concurrency/concurrency7.cr
@@ -0,0 +1,31 @@
+class SafeCounter
+  property v, mux
+
+  def initialize
+    @v = Hash(String, Int32).new
+    @mux = Mutex.new
+  end
+
+  def inc(key : String)
+    mux.lock
+    v[key] = v.has_key?(key) ? v[key] + 1 : 1
+    mux.unlock
+  end
+
+  def value(key : String)
+    mux.lock
+    v[key]
+  ensure
+    mux.unlock
+  end
+end
+
+c = SafeCounter.new
+(1..1000).each {
+  spawn do
+    c.inc("someKey")
+  end
+}
+
+sleep 1.second
+puts c.value("someKey")
